3ba73bac9f7347c515b7131c5a6625498a69a472
SilviuDN <silviudilimot@gmail.com>
2023-04-15 11:14:24 +0200

    hidden the about

--- src/components/pages/CoursesPage/CourseCard.js
+++ src/components/pages/CoursesPage/CourseCard.js
@@ -6,14 +6,14 @@ import {Link} from 'react-router-dom'
 
 const CourseCard = ({name, about, description, image, _id, reviews, loggedUser}) => {
     // const shortDescription = description[0].slice(0, 70)
-    const shortDescription =about.slice(0, 70)
+    const shortDescription = about.slice(0, 70)
 
     return(
         <Col sm={6} lg={4}>
             <article className="courseCard">
                 <img src={image} alt={name}/>
                 <h4>{name}</h4>
-                <p>{shortDescription}...</p>
+                {/* <p>{shortDescription}...</p> */}
                 <Link to={`/courses/details/${_id}`}>
                     <Button className="btnBlock" variant="secondary">Detalii</Button>
                 </Link>
